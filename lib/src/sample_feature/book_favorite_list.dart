b21fe883ab086f198d83f2058f9fc6eebea55a76
Serdar Dev <serdardev2@gmail.com>
2024-02-10 22:32:43 +0300

    Add favorite book list functionality

--- lib/src/sample_feature/book_favorite_list.dart
+++ lib/src/sample_feature/book_favorite_list.dart
@@ -1,57 +1,106 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_case/src/sample_feature/book_item_details_view.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert';
+import 'styles.dart';
 
-import '../settings/settings_view.dart';
-import 'book_item.dart';
-import 'book_item_details_view.dart';
-
-/// Displays a list of BookFavorites.
-class BookFavoriteListView extends StatelessWidget {
-  const BookFavoriteListView({
-    super.key,
-    this.items = const [BookItem(1), BookItem(2), BookItem(3)],
-  });
-
+class BookFavoriteListView extends StatefulWidget {
+  const BookFavoriteListView({Key? key}) : super(key: key);
   static const routeName = '/';
+  @override
+  BookFavoriteListViewState createState() => BookFavoriteListViewState();
+}
 
-  final List<BookItem> items;
+class BookFavoriteListViewState extends State<BookFavoriteListView> {
+  final _list = <dynamic>[];
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      loadFavorites();
+    });
+  }
+
+  void _navigateToDetails(BuildContext context, Map<String, dynamic> args) {
+    Navigator.restorablePushNamed(
+      context,
+      BookItemDetailsView.routeName,
+      arguments: args,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Book Items'),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.settings),
-            onPressed: () {
-              Navigator.restorablePushNamed(context, SettingsView.routeName);
-            },
+        title: const Text('Favorilerim'),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: _list.length,
+              itemBuilder: (BuildContext context, int index) {
+                dynamic data = _list[index];
+                String id = data['id'];
+                String smallThumbnail =
+                    (data?['smallThumbnail'] as String?) ?? '';
+                String title = (data?['title'] as String?) ?? '';
+                String subTitle = (data?['subTitle'] as String?) ?? '';
+
+                return ListTile(
+                  leading: Container(
+                    width: 60.0,
+                    height: 60.0,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8.0),
+                      image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: NetworkImage(smallThumbnail),
+                      ),
+                    ),
+                  ),
+                  title: Text(
+                    title,
+                    style: MyStyles.titleStyle,
+                    maxLines: 2,
+                  ),
+                  subtitle: Text(
+                    subTitle,
+                    style: MyStyles.subtitleStyle,
+                  ),
+                  onTap: () {
+                    _navigateToDetails(
+                      context,
+                      {
+                        'id': id,
+                        'title': title,
+                        'subTitle': subTitle,
+                        'smallThumbnail': smallThumbnail,
+                      },
+                    );
+                  },
+                );
+              },
+            ),
           ),
         ],
       ),
-      body: ListView.builder(
-        restorationId: 'bookFavoriteListView',
-        itemCount: items.length,
-        itemBuilder: (BuildContext context, int index) {
-          final item = items[index];
-
-          return ListTile(
-              title: Text('BookFavorite ${item.id}'),
-              leading: const CircleAvatar(
-                // Display the Flutter Logo image asset.
-                foregroundImage: AssetImage('assets/images/flutter_logo.png'),
-              ),
-              onTap: () {
-                // Navigate to the details page. If the user leaves and returns to
-                // the app after it has been killed while running in the
-                // background, the navigation stack is restored.
-                Navigator.restorablePushNamed(
-                  context,
-                  BookItemDetailsView.routeName,
-                );
-              });
-        },
-      ),
     );
+  }
+
+  Future<List<dynamic>> getFavorites() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String favorites = prefs.getString('favorites') ?? '[]';
+    return jsonDecode(favorites);
+  }
+
+  Future<void> loadFavorites() async {
+    List<dynamic> favorites = await getFavorites();
+    setState(() {
+      _list.clear();
+      _list.addAll(favorites);
+    });
   }
 }
